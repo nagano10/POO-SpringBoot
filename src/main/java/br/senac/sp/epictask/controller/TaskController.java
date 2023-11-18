05538aecca6287e3ec6b6ea6adb48af4101efcf4
EDUARDO NAGANO LACERDA <eduardo.nlacerda@senacsp.edu.br>
2023-11-17 21:53:08 -0300

    criacao de tarefa

--- src/main/java/br/senac/sp/epictask/controller/TaskController.java
+++ src/main/java/br/senac/sp/epictask/controller/TaskController.java
@@ -1,10 +1,14 @@
 package br.senac.sp.epictask.controller;
 
+import br.senac.sp.epictask.model.Task;
 import br.senac.sp.epictask.repository.TaskRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,5 +25,21 @@ public class TaskController {
         return "task/index";
 
     }
+
+    @GetMapping("new") //localhost:8080/task/new
+    public String form(Task task){
+
+        return "task/form";
+
+    }
+
+    @PostMapping("new")
+    public String save(@Valid Task task, BindingResult result){
+        if(result.hasErrors()) return "task/form";
+
+        repository.save(task);
+        return "redirect:/task";
+    }
+
 
 }
--- src/main/java/br/senac/sp/epictask/model/Task.java
+++ src/main/java/br/senac/sp/epictask/model/Task.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 //Mapeamento objeto relacionado - ORM
@@ -13,8 +14,18 @@ import lombok.Data;
 public class Task {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @NotBlank
     String title;
+
+    //NotNull - aceita por exemplo ""
+    //"" NotEmpty
+    //"   " NotBlank
+
+    @Size(min = 10, message = "Digite no mínimo 10 caracteres.")
     String description;
+
+    @Min(value = 1, message = "Digite um número maior ou igual a um.")
     Integer score;
 
 

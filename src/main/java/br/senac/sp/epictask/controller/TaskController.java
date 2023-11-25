6cac05d71bc2a95a6558a32983d49d9d422afd44
EDUARDO NAGANO LACERDA <eduardo.nlacerda@senacsp.edu.br>
2023-11-24 21:30:58 -0300

    deu bom

--- src/main/java/br/senac/sp/epictask/controller/TaskController.java
+++ src/main/java/br/senac/sp/epictask/controller/TaskController.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("task")
@@ -39,6 +37,15 @@ public class TaskController {
 
         repository.save(task);
         return "redirect:/task";
+    }
+
+    @DeleteMapping("{id}") // /task/{id}
+    public String delete(@PathVariable Long id){
+
+        repository.deleteById(id);
+
+        return "redirect:/task";
+
     }
 
 
